c4cfc0329b23f3e3ddadbdf5565a5b5c30fd7fd7
Nick Heiner <nheiner@netflix.com>
2021-09-01 15:13:57 -0400

    Simplify, yet the issue still repros

--- cases/piscina-perf/worker.js
+++ cases/piscina-perf/worker.js
@@ -1,43 +1,20 @@
-require('recast');
 const createLog = require('nth-log').default;
-const fs = require('fs');
-// const path = require('path');
-// const piscina = require('piscina');
 
 const babel = require('@babel/core');
 
 const log = createLog({name: 'piscina-perf-worker'});
 
-// function generateRandomString(length) {
-//   const chars = [];
-//   for (let i = 0; i < length; i++) {
-//     chars.push(String(Math.random() * length));
-//   }
-//   return chars.join('');
-// }
-
 module.exports = async function(filePath) {
   return log.logPhase({filePath, phase: 'worker', level: 'debug'}, async () => {
     try {
-      const fileContents = await fs.promises.readFile(filePath, 'utf8');
-      const compiled = babel.transformSync(fileContents, {
+      babel.loadOptions({
         filename: filePath,
         presets: [['babel-preset-tvui', { modules: false, appName: '' }]],
         cwd: '/Users/nheiner/code/tvui/'
       });
     } catch (e) {
+      console.log(e);
       log.info({e}, 'Worker threw error');
     }
-  //   const filePath = path.join(piscina.workerData.generatedDirPath, `${taskId}.txt`);
-  //   await fs.promises.writeFile(
-  //     filePath, 
-  //     generateRandomString(1000)
-  //   );
-
-  //   await fs.promises.readFile(filePath, 'utf8');
-  // })
-  // const iterationLimit = 5 * (10 ** 9);
-  // log.logPhase({phase: 'empty for loop', iterationLimit, level: 'info'}, () => {
-  //   for (let i = 0; i < iterationLimit; i++) {}
   });
 }
