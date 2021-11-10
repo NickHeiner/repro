580bb6baa071cdfb26437cacf8e56ee6eb9cdf01
Nick Heiner <nheiner@netflix.com>
2021-11-10 17:02:44 -0500

    Adding another fix

--- cases/ts-global-this/index.d.ts
+++ cases/ts-global-this/index.d.ts
@@ -1,6 +1,11 @@
-export {};
+// Option 1:
+// export {};
 
-declare global {
-  const console: any;
-  var myCustomGlobalThisEntry: string;
-}
+// declare global {
+//   const console: any;
+//   var myCustomGlobalThisEntry: string;
+// }
+
+// Option 2:
+declare const console: any;
+declare var myCustomGlobalThisEntry: string;
